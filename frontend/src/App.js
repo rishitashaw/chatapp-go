a87eac5765c1a3a8ef0d62e341229d08da022dc2
Rishita Shaw <rishitashaw49@gmail.com>
2022-01-27 00:35:23 +0530

    api integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,189 +1,68 @@
-/* eslint-disable jsx-a11y/alt-text */
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 
 function App () {
+  const [ username, setUsername ] = useState( 'username' );
+  const [ messages, setMessages ] = useState( [] );
+  const [ message, setMessage ] = useState( '' );
+  let allMessages = [];
+
+  useEffect( () => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher( '', {
+      cluster: ''
+    } );
+
+    const channel = pusher.subscribe( 'chat' );
+    channel.bind( 'message', function ( data ) {
+      allMessages.push( data );
+      setMessages( allMessages );
+    } );
+  }, [] );
+
+  const submit = async e => {
+    e.preventDefault();
+
+    await fetch( 'http://localhost:8000/api/messages', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify( {
+        username,
+        message
+      } )
+    } );
+
+    setMessage( '' );
+  }
+
   return (
-
-    <body>
-      <div class="container-fluid h-100 mt-5">
-        <div class="row justify-content-center h-100">
-          <div class="col-md-4 col-xl-3 chat"><div class="card mb-sm-3 mb-md-0 contacts_card">
-            <div class="card-header">
-              <div class="input-group">
-                <input type="text" placeholder="Search..." name="" class="form-control search" />
-                <div class="input-group-prepend">
-                  <span class="input-group-text search_btn"><i class="fas fa-search"></i></span>
+    <div className="container">
+      <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+        <div
+          className="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+          <input className="fs-5 fw-semibold" value={ username }
+            onChange={ e => setUsername( e.target.value ) } />
+        </div>
+        <div className="list-group list-group-flush border-bottom scrollarea">
+          { messages.map( message => {
+            return (
+              <div className="list-group-item list-group-item-action py-3 lh-tight">
+                <div className="d-flex w-100 align-items-center justify-content-between">
+                  <strong className="mb-1">{ message.username }</strong>
                 </div>
+                <div className="col-10 mb-1 small">{ message.message }</div>
               </div>
-            </div>
-            <div class="card-body contacts_body">
-              <ui class="contacts">
-                <li class="active">
-                  <div class="d-flex bd-highlight">
-                    <div class="img_cont">
-                      <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img" />
-                      <span class="online_icon"></span>
-                    </div>
-                    <div class="user_info">
-                      <span>Khalid</span>
-                      <p>Kalid is online</p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="d-flex bd-highlight">
-                    <div class="img_cont">
-                      <img src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg" class="rounded-circle user_img" />
-                      <span class="online_icon offline"></span>
-                    </div>
-                    <div class="user_info">
-                      <span>Taherah Big</span>
-                      <p>Taherah left 7 mins ago</p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="d-flex bd-highlight">
-                    <div class="img_cont">
-                      <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" class="rounded-circle user_img" />
-                      <span class="online_icon"></span>
-                    </div>
-                    <div class="user_info">
-                      <span>Sami Rafi</span>
-                      <p>Sami is online</p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="d-flex bd-highlight">
-                    <div class="img_cont">
-                      <img src="http://profilepicturesdp.com/wp-content/uploads/2018/07/sweet-girl-profile-pictures-9.jpg" class="rounded-circle user_img" />
-                      <span class="online_icon offline"></span>
-                    </div>
-                    <div class="user_info">
-                      <span>Nargis Hawa</span>
-                      <p>Nargis left 30 mins ago</p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="d-flex bd-highlight">
-                    <div class="img_cont">
-                      <img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" class="rounded-circle user_img" />
-                      <span class="online_icon offline"></span>
-                    </div>
-                    <div class="user_info">
-                      <span>Rashid Samim</span>
-                      <p>Rashid left 50 mins ago</p>
-                    </div>
-                  </div>
-                </li>
-              </ui>
-            </div>
-            <div class="card-footer"></div>
-          </div></div>
-          <div class="col-md-8 col-xl-6 chat">
-            <div class="card">
-              <div class="card-header msg_head">
-                <div class="d-flex bd-highlight">
-                  <div class="img_cont">
-                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img" />
-                    <span class="online_icon"></span>
-                  </div>
-                  <div class="user_info">
-                    <span>Chat with Khalid</span>
-                    <p>1767 Messages</p>
-                  </div>
-                  <div class="video_cam">
-                    <span><i class="fas fa-video"></i></span>
-                    <span><i class="fas fa-phone"></i></span>
-                  </div>
-                </div>
-                <span id="action_menu_btn"><i class="fas fa-ellipsis-v"></i></span>
-                <div class="action_menu">
-                  <ul>
-                    <li><i class="fas fa-user-circle"></i> View profile</li>
-                    <li><i class="fas fa-users"></i> Add to close friends</li>
-                    <li><i class="fas fa-plus"></i> Add to group</li>
-                    <li><i class="fas fa-ban"></i> Block</li>
-                  </ul>
-                </div>
-              </div>
-              <div class="card-body msg_card_body">
-                <div class="d-flex justify-content-start mb-4">
-                  <div class="img_cont_msg">
-                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
-                  </div>
-                  <div class="msg_cotainer">
-                    Hi, how are you samim?
-                    <span class="msg_time">8:40 AM, Today</span>
-                  </div>
-                </div>
-                <div class="d-flex justify-content-end mb-4">
-                  <div class="msg_cotainer_send">
-                    Hi Khalid i am good tnx how about you?
-                    <span class="msg_time_send">8:55 AM, Today</span>
-                  </div>
-
-                </div>
-                <div class="d-flex justify-content-start mb-4">
-                  <div class="img_cont_msg">
-                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
-                  </div>
-                  <div class="msg_cotainer">
-                    I am good too, thank you for your chat template
-                    <span class="msg_time">9:00 AM, Today</span>
-                  </div>
-                </div>
-                <div class="d-flex justify-content-end mb-4">
-                  <div class="msg_cotainer_send">
-                    You are welcome
-                    <span class="msg_time_send">9:05 AM, Today</span>
-                  </div>
-
-                </div>
-                <div class="d-flex justify-content-start mb-4">
-                  <div class="img_cont_msg">
-                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
-                  </div>
-                  <div class="msg_cotainer">
-                    I am looking for your next templates
-                    <span class="msg_time">9:07 AM, Today</span>
-                  </div>
-                </div>
-                <div class="d-flex justify-content-end mb-4">
-                  <div class="msg_cotainer_send">
-                    Ok, thank you have a good day
-                    <span class="msg_time_send">9:10 AM, Today</span>
-                  </div>
-
-                </div>
-                <div class="d-flex justify-content-start mb-4">
-                  <div class="img_cont_msg">
-                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg" />
-                  </div>
-                  <div class="msg_cotainer">
-                    Bye, see you
-                    <span class="msg_time">9:12 AM, Today</span>
-                  </div>
-                </div>
-              </div>
-              <div class="card-footer">
-                <div class="input-group">
-                  <div class="input-group-append">
-                    <span class="input-group-text attach_btn"><i class="fas fa-paperclip"></i></span>
-                  </div>
-                  <textarea name="" class="form-control type_msg" placeholder="Type your message..."></textarea>
-                  <div class="input-group-append">
-                    <span class="input-group-text send_btn"><i class="fas fa-location-arrow"></i></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            )
+          } ) }
         </div>
       </div>
-    </body>
-
+      <form onSubmit={ e => submit( e ) }>
+        <input className="form-control" placeholder="Write a message" value={ message }
+          onChange={ e => setMessage( e.target.value ) }
+        />
+      </form>
+    </div>
   );
 }
 
